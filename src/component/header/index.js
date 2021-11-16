47871721441bb870ac70466456ce4d4cc7ff2a74
Uttam Marandi <uttammarandi007@gmail.com>
2021-11-16 12:40:57 +0530

    header component withou test

---
+++ src/component/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">Test Enzyme</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
