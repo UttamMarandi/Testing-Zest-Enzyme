62479b5a6d881a77e1fb7f627f782c8ac2e8a84a
Uttam Marandi <uttammarandi007@gmail.com>
2021-11-18 18:34:51 +0530

    posts Reducer

--- src/createStore.js
+++ src/createStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import ReduxThunk from "redux-thunk";
 
-export const middlewares = [ReduxThunk];
+export const middlewares = [ReduxThunk]; //redux thunk is milddleware
 
 export const createStoreWithMiddleware = applyMiddleware(...middlewares)(
   createStore
@@ -11,3 +11,8 @@ export const createStoreWithMiddleware = applyMiddleware(...middlewares)(
 export const store = createStoreWithMiddleware(rootReducer);
 
 // export const store = createStore(rootReducer);
+
+//reducers are pure functions. Two types pure and inpure functions
+//pure functions meaning the function will return a new state.
+//reducer should not have any complex logic.
+//you should only take the payload and return a new state.
